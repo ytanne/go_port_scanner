c233652d24efb9c516815f33c61fa571fa9936d9
fl4ssh <fl4ssh>
2021-05-18 14:23:10 +0600

    Removed service detection

--- pkg/repository/nmap.go
+++ pkg/repository/nmap.go
@@ -16,7 +16,7 @@ func (ps *NmapScanner) ScanPorts(target string) ([]byte, error) {
 }
 
 func getPorts(address string) ([]byte, error) {
-	return exec.Command("nmap", "--top-ports", "10000", "-sV", "-T3", address).Output()
+	return exec.Command("nmap", "--top-ports", "10000", "-T3", address).Output()
 }
 
 func (ns *NmapScanner) ScanNetwork(target string) ([]byte, error) {
